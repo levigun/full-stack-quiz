206b10036aa375762f48ac352c926573f7209256
levigun <t.levina31@gmail.com>
2021-12-24 20:42:49 +1100

    pseudo coding

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// Pseudo coding
+// 1. user will open link and see the quiz page
+// 2. there will be a start button present and the user will click to start the quiz.
+// 3. Questions will be presented but the questions are displayed one by one.
+// 4. Upon the starting button, the timer of 90s will also start with the questions.
+// 5. the user will answer a question.
+// 6. if they answer correctly, it will go to the next question.
+// 7. if the naswer is wrong an alert will be displayed and it will say "Wrong answer! you have lost 10s of your time"
+// 8. then the next question will be displayed.
+// 9. When alll the answers are answered or the timer is 0, the game is over.
+// 10. when the game is over, the user can save their score and input their initials.
+// 11. then in the high score page they can choose to view the high scores or go back and start the game again.
+// 11. The user can also check their previous scores and intials when the "view highscore" 
+//  link is clicked at the top of the first page. (this means it will be saved in local storage)
