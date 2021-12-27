169dc9fde1cf67ed168f89412483ed68149e8ebe
levigun <t.levina31@gmail.com>
2021-12-27 18:23:20 +1100

    made a seperate file for pseudo code draft

---
+++ assets/pseudocode.js
@@ -0,0 +1,15 @@
+// Pseudo coding
+// 1. user will open link and see the quiz page
+// 2. there will be a start button present and the user will click to start the quiz.
+// 3. Upon the starting button, the timer of 90s will also start with the questions.
+// 4. the user will answer one question at a time.
+// 5. if they answer correctly, it will go to the next question.
+// 6. if the answer is wrong an alert will be displayed and it will say "Wrong answer! you have lost 10s of your time"
+// 7. then the next question will be displayed.
+// 8. When alll the answers are answered or the timer is 0, the game is over.
+// 9. when the game is over, the user can save their score and input their initials.
+// 10. then in the high score page they can choose to view the high scores or go back and start the game again.
+// 11. The user can also check their previous scores and intials when the "view highscore" 
+//  link is clicked at the top of the first page. (this means it will be saved in local storage)
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,128 @@
-// Pseudo coding
-// 1. user will open link and see the quiz page
-// 2. there will be a start button present and the user will click to start the quiz.
+var startButtonEl = $('.btn-container');
+var q1ButtoneEl = $("button[name='q1']")
+var score = 0;
+var total = 5;
+var point = 1;
+var highest = total * point;
 
-var startButton = document.querySelector("#start-quiz");
 
-startButton.addEventListener("click", function(){
 
+// hides all the questions,high score page, and the "All Done "section
+$(document).ready(function(){
+    $('.question').hide();
+    $('#all-done').hide();
+    $('#view-highscore').hide();
 
 })
 
-// 3. Questions will be presented but the questions are displayed one by one.
-// 4. Upon the starting button, the timer of 90s will also start with the questions.
-// 5. the user will answer a question.
-// 6. if they answer correctly, it will go to the next question.
-// 7. if the naswer is wrong an alert will be displayed and it will say "Wrong answer! you have lost 10s of your time"
-// 8. then the next question will be displayed.
-// 9. When alll the answers are answered or the timer is 0, the game is over.
-// 10. when the game is over, the user can save their score and input their initials.
-// 11. then in the high score page they can choose to view the high scores or go back and start the game again.
-// 11. The user can also check their previous scores and intials when the "view highscore" 
-//  link is clicked at the top of the first page. (this means it will be saved in local storage)
+// when start button is clicked, 1st question is showing
+//Questions will then  be presented but the questions are displayed one by one.
+
+     $('.btn-container').click(function() {
+        $('#q1').show();
+        $('.intro').hide();
+        return false;
+        });
+
+    q1ButtoneEl.on('click', function(event){
+        if (q1ButtoneEl[value="c"]){
+        // how to target the buttone value
+        $('#q2').show();
+        $('#q1').hide();
+        } else {
+        alert("Wrong answer! you have lost 10s of your time")
+        $('#q2').show();
+        $('#q1').hide();
+        event.preventDefault();
+
+    }
+    });
+
+// $("button[name='q1']").click(function() {
+//     $('#q2').show();
+//     $('#q1').hide();
+//     return false;
+// });
+
+    $("button[name='q2']").click(function() {
+        $('#q3').show();
+        $('#q2').hide();
+        return false;
+    });
+
+    $("button[name='q3']").click(function() {
+        $('#q4').show();
+        $('#q3').hide();
+        return false;
+    });
+
+    $("button[name='q4']").click(function() {
+        $('#q5').show();
+        $('#q4').hide();
+        return false;
+    });
+
+    $("button[name='q5']").click(function() {
+        $('#all-done').show();
+        $('#q5').hide();
+        return false;
+    });
+
+    $('#submit').click(function() {
+        $('#view-highscore').show();
+        $('#all-done').hide();
+        return false;
+    });
+
+    $('#goBack').click(function() {
+        $('.intro').show();
+        $('#view-highscore').hide();
+        return false;
+    });
+
+// // Initializer
+// function init ();
+// // set correct answers
+//     localStorage.setItem('a1','c');
+//     localStorage.setItem('a2','c');
+//     localStorage.setItem('a3','d');
+//     localStorage.setItem('a4','c');
+//     localStorage.setItem('a5','d');
+
+
+// // Add event listener
+// window.addEventListener('load',init,false);
+
+// $('#clearHighscore').click(function() {
+//     // clears the highscore function
+// });
+
+
+
+// when the start button is clicked, the timer starts
+// var secondsLeft= 90;
+// var timeEl = $('float-right');
+    
+// startButtonEl.on('click', function () {  
+//     function setTime() {
+//         // Sets interval in variable
+//         var timerInterval = setInterval(function() {
+//           secondsLeft--;
+//           timeEl.textContent = "Time: " + secondsLeft + " s";
+      
+//           if(secondsLeft === 0) {
+//             // Stops execution of action at set interval
+//             clearInterval(timerInterval);
+//           }
+      
+//         }, 1000);
+//       }
+//       setTime ();
+// }
+//  ask question about the timer change to Time: 90s then the 90s --;
+
+
+
 
 
 
